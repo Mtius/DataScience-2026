44ae32f4b58249bfb9f0881ca83b96b0b993e57e
Santiago Matheus <smatheus@unab.edu.co>
2026-02-25 18:45:03 -0500

    :wq

---
+++ Leer_imagenes.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+ 
+# cargar CSV
+image = np.loadtxt("numero8255.csv", delimiter=";", skiprows=1)
+ 
+ 
+# mostrar imagen
+plt.imshow(image, cmap='gray', vmin=0, vmax=255)
+plt.axis('off')
+plt.show()
+ 
+ 
